ff004a9ea8a79ba79d6aa5ffbda017c358eb2130
MitchellGerdisch <mitch@pulumi.com>
2023-12-19 13:28:57 -0600

    expiration tag

--- provider/cmd/pulumi-resource-stackmgmt/stackSettings.ts
+++ provider/cmd/pulumi-resource-stackmgmt/stackSettings.ts
@@ -21,39 +21,31 @@ export class StackSettings extends pulumi.ComponentResource {
     const project = pulumi.getProject()
     const stack = pulumi.getStack()
 
-    //// Set stack tag for TTL
-    // The TTL processor looks for a "ttl" stack tag set to the number of minutes to run.
-    // If not explicitly set as a configuration property, set the ttl to destroy today at 11:59PM UTC.
+    //// Set stack tag for expiration.
+    // The stack manager processor looks for a "expiration" stack tag set to the time after which the stack should be destroyed.
+    // If not explicitly set as a configuration property, add 8 hours to now.
     let ttlMinutes = args.ttlMinutes
     if (!ttlMinutes) {
-      ttlMinutes = Math.round((new Date().setHours(24,0,0,0) - Date.now()) / 60 / 1000)
-
-      /// Some older code that calculated TTL minutes until Friday at midnight.
-      // const dayOfWeek = 5 // Friday
-      // const nowDate = new Date() // Right now
-      // const destroyDate = new Date() // will be modified to represent the target destroy date/time.
-      // // Set date to this coming Friday
-      // destroyDate.setDate(nowDate.getDate() + ((dayOfWeek + 7 - nowDate.getDay()) % 7))
-      // // Set time to 11PM
-      // destroyDate.setUTCHours(23) // 11PM UTC
-      // destroyDate.setMinutes(0) 
-      // destroyDate.setMilliseconds(0) 
-      // Do some math
-      // ttlMinutes = Math.round((destroyDate.getTime() - nowDate.getTime()) / (60000))
-
-      // Shouldn't happen but if this runs near the cusp of midnight, we might get a negative number.
-      // So let the stack live for a couple of hours.
-      if (ttlMinutes < 0) { ttlMinutes = 120 }
+      ttlMinutes = (8 * 60)
     }
+    // Calculate milliseconds to add for linux time math
+    const millisecondsToAdd = ttlMinutes * 60 * 1000
 
-    // This stack tag tells the TTL stack how long it should wait before destroying the tagged stack.
-    const ttlStackTag = new pulumiservice.StackTag(`${name}-ttl-stacktag`, {
+    // Calculate the UTC time after which this stack should be destroyed.
+    const nowTime = new Date()
+    const nowLinuxTime = nowTime.getTime()
+    const endLinuxTime = nowLinuxTime + millisecondsToAdd
+    const endDate = new Date(endLinuxTime)
+    const expirationTagSetting = endDate.toISOString()
+
+    // This stack tag tells the management service the time after which this stack should be terminated.
+    const expirationStackTag = new pulumiservice.StackTag(`${name}-ttl-stacktag`, {
       organization: org,
       project: project,
       stack: stack,
-      name: "ttl",
-      value: ttlMinutes.toString(),
-    }, { parent: this })
+      name: "expiration",
+      value: expirationTagSetting
+    }, { parent: this, ignoreChanges: ["value"] })
 
     // This stack tag tells the Drift Correction stack that this stack should be refreshed.
     const refreshStackTag = new pulumiservice.StackTag(`${name}-driftmanagement-stacktag`, {
