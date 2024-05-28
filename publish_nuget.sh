191ab88adb9b14e763707b66760d8822b2ee3f50
MitchellGerdisch <mitch@pulumi.com>
2024-05-28 12:49:47 -0500

    clean up publish_nuget script

--- publish_nuget.sh
+++ publish_nuget.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-  echo "Usage $0 VERSION_NUMBER"
-  echo "Example $0 3.1.3"
+  echo "Usage $0 SOURCE_NAME VERSION_NUMBER"
+  echo "Where SOURCE_NAME is the package name and VERSION_NUMBER is the package version (minus \"v\")"
+  echo "Example $0 Pequod.Stackmgmt 3.1.3"
   exit 1
 fi
 
-PKG_BASE_NAME="Pequod.Stackmgmt"
-NUGET_PKG="${PKG_BASE_NAME}.${1}.nupkg"
-SOURCE_NAME="github"
+SOURCE_NAME=${1}
+NUGET_PKG="${PKG_BASE_NAME}.${2}.nupkg"
 GITHUB_PACKAGE_URL="https://nuget.pkg.github.com/pulumi-pequod/index.json"
 
-echo "Adding gihtub package source"
-# echo "dotnet nuget add source --username ${GITHUB_PACKAGE_TOKEN_USERNAME} --password ${GITHUB_PACKAGE_TOKEN} --store-password-in-clear-text --name ${SOURCE_NAME} ${GITHUB_PACKAGE_URL}"
-dotnet nuget add source --username ${GITHUB_PACKAGE_TOKEN_USERNAME} --password ${GITHUB_PACKAGE_TOKEN} --store-password-in-clear-text --name ${SOURCE_NAME} ${GITHUB_PACKAGE_URL}
-dotnet nuget list source
+# See if the source is already configured
+dotnet nuget list source | grep $SOURCE_NAME -q
+if [ $? -ne 0 ]
+then
+  dotnet nuget add source --username ${GITHUB_PACKAGE_TOKEN_USERNAME} --password ${GITHUB_PACKAGE_TOKEN} --store-password-in-clear-text --name ${SOURCE_NAME} ${GITHUB_PACKAGE_URL}
+else
+  echo "$SOURCE_NAME is already added as a source"
+fi
 
 echo "Publishing nuget package ${NUGET_PKG}"
-# echo "dotnet nuget push ./nuget/Pulumi.Stackmgmt.${1}.nupkg  --api-key $GITHUB_PACKAGE_TOKEN --source github"
-dotnet nuget push ./sdk/dotnet/bin/Debug/*.nupkg --api-key $GITHUB_PACKAGE_TOKEN --source github
+dotnet nuget push ./sdk/dotnet/bin/Debug/*.nupkg --api-key ${GITHUB_PACKAGE_TOKEN} --source ${SOURCE_NAME}
